d9f14a2e0c70a643422ca55cfe5029dca84e21b5
Fernando Guillen <fguillen.mail@gmail.com>
2016-05-05 18:06:50 +0200

    Assault Controller (II)

--- app/controllers/assault.js
+++ app/controllers/assault.js
@@ -1,11 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  userResponse: null,
+  userAnswer: null,
 
   actions: {
     resolveAssault() {
-      console.log("XXX: userResponse", this.get('userResponse'));
+      console.log("XXX: userAnswer", this.get('userAnswer'));
+      this.set('model.userAnswer', this.get('userAnswer'));
+
+      if(this.get('model.config.answer') == this.get('userAnswer')){
+        console.log('XXX: User response correct!');
+        this.set('model.result', true);
+      } else {
+        console.log('XXX: User response error');
+        this.set('model.result', false);
+      }
+
+      console.log('XXX: model', this.get('model'));
     }
   }
 });
